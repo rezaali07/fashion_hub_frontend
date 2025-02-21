a459199f74f71226357ee283194b8d548171511e
rezaali2290 <rezaali20ab@gmail.com>
2025-02-21 16:33:59 +0545

    error fixed on store

--- src/Store.js
+++ src/Store.js
@@ -54,5 +54,14 @@ let initialState = {
   },
 };
 
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+
+  initialState,
+
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
