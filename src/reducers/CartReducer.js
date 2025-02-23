fcfe72841b6579d36a22bc403d141914c5ebc35d
rezaali2290 <rezaali20ab@gmail.com>
2025-02-23 16:22:12 +0545

    wokerd for cart reducer

---
+++ src/reducers/CartReducer.js
@@ -0,0 +1,50 @@
+import {
+  ADD_TO_CART,
+  REMOVE_CART_ITEM,
+  SAVE_SHIPPING_INFO,
+  CLEAR_CART, // Add this new constant
+} from "../constants/CartConstants";
+
+export const cartReducer = (
+  state = { cartItems: [], shippingInfo: {} },
+  action
+) => {
+  switch (action.type) {
+    case ADD_TO_CART: {
+      const item = action.payload;
+
+      // Check if the item already exists in the cart
+      const isItemExist = state.cartItems.find(
+        (i) => i.product === item.product
+      );
+
+      if (isItemExist) {
+        // Update the existing item
+        return {
+          ...state,
+          cartItems: state.cartItems.map((i) =>
+            i.product === isItemExist.product ? item : i
+          ),
+        };
+      } else {
+        // Add a new item to the cart
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+    }
+
+    
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartItems: [], // Clear cart items
+        shippingInfo: {}, // Optionally clear shipping info
+      };
+
+    default:
+      return state;
+  }
+};
+
