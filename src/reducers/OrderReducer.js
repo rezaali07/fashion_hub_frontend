f3551a097fc6a387136e801c5352507e6718b312
rezaali2290 <rezaali20ab@gmail.com>
2025-02-23 16:25:49 +0545

    All Orders for admin

--- src/reducers/OrderReducer.js
+++ src/reducers/OrderReducer.js
@@ -82,6 +82,36 @@ export const myOrdersReducer = (state = { orders: [] }, action) => {
 };
 
 
+// All Orders ------ Admin
+export const allOrdersReducer = (state = { orders: [] }, action) => {
+  switch (action.type) {
+    case ALL_ORDERS_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case ALL_ORDERS_SUCCESS:
+      return {
+        loading: false,
+        orders: action.payload,
+      };
+
+    case ALL_ORDERS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
 export const orderReducer = (state = {}, action) => {
   switch (action.type) {
     case UPDATE_ORDER_REQUEST:
